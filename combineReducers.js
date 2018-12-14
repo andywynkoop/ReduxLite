c284a8c240dc19243d6f9065858459bc8ef0b7e5
Andy Wynkoop <andywynkoop@gmail.com>
2018-12-13 23:20:47 -0800

    Fix nested state broadcast issue in combineReducers

--- combineReducers.js
+++ combineReducers.js
@@ -1,16 +1,30 @@
-export default config => (oldState, action, subscriptions) => {
-  const nextState = {};
-  let stateChanged = false;
-  Object.keys(config).forEach(key => {
-    const reducer = config[key];
-    let oldSlice = oldState[key];
-    const newSlice = reducer(oldSlice, action, subscriptions);
-    if (!Object.is(oldSlice, newSlice)) {
-      stateChanged = true;
-    }
-    nextState[key] = newSlice;
-  });
-  
-  if (stateChanged) subscriptions.forEach(cb => cb(nextState));
-  return nextState;
+export default config => {
+  const _reducer = (oldState, action, subscriptions) => {
+    const nextState = {};
+    let stateChanged = false;
+    Object.keys(config).forEach(key => {
+      const reducer = config[key];
+      let oldSlice;
+      if (action.type === "__initializeStore" && oldState[key] === undefined) {
+        if (reducer.type === "combine") {
+          oldSlice = reducer({}, action);
+        } else {
+          let args = [, action];
+          oldSlice = reducer(...args);
+        }
+      } else {
+        oldSlice = oldState[key];
+      }
+      const newSlice = reducer(oldSlice, action, []);
+      if (!Object.is(oldSlice, newSlice)) {
+        stateChanged = true;
+      }
+      nextState[key] = newSlice;
+    });
+
+    if (stateChanged) subscriptions.forEach(cb => cb(nextState));
+    return nextState;
+  }
+  _reducer.type = "combine";
+  return _reducer;
 }
